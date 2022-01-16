24869707210bf1e7cfc88eaf849663fdb971b40f
SayanDasgupta <sayan.dasgupta.2000@gmail.com>
2022-01-17 00:14:32 +0530

    Working Somewhat

--- ArduinoCodes/sketch_jan16a/sketch_jan16a.ino
+++ ArduinoCodes/sketch_jan16a/sketch_jan16a.ino
@@ -1,6 +1,15 @@
 #include <LiquidCrystal.h>
+#include <SoftwareSerial.h>
 
 LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+SoftwareSerial sim800l(0, 1);
+
+/*
+ * For Impulse Rate : 
+ * 6400 -> 562.5
+ * 3200 -> 1125
+ * 1600 -> 2250
+ */
 
 //variables
 unsigned int sum, prevTime, count;
@@ -11,8 +20,12 @@ void setup() {
   // functional Setup
   lcd.begin(16,2);
   pinMode(A0,INPUT);
-  pinMode(0,INPUT_PULLUP);
+  pinMode(6,INPUT_PULLUP);
   pinMode(7, OUTPUT);
+
+  sim800l.begin(9600);   //Module baude rate, this is on max, it depends on the version
+  Serial.begin(9600);   
+  delay(1000);
   
   //innitialize
   sum = 0;
@@ -39,7 +52,16 @@ void loop() {
     digitalWrite(7, LOW);
     flag=false;
   }
-  
+
+  //EVENT : Button Press
+  if (digitalRead(6) == LOW) {            //And if it's pressed
+    Serial.println("Button pressed");   //Shows this message on the serial monitor
+    delay(200);                         //Small delay to avoid detecting the button press many times
+    SendSMS(getWattConsumed());                          //And this function is called
+  }
+  if (sim800l.available()){            //Displays on the serial monitor if there's a communication from the module
+    Serial.write(sim800l.read()); 
+  }
 }
 
 // Custom Functions 
@@ -58,4 +80,20 @@ float getAvgBlinkTime(){
 float getWattConsumed(){
   float avgBlink = getAvgBlinkTime();
   return (avgBlink==0.0)?0.0:(impulseRate/avgBlink);
+}
+
+void SendSMS(float val){
+  Serial.println("Sending SMS...");               //Show this message on serial monitor
+  sim800l.print("AT+CMGF=1\r");                   //Set the module to SMS mode
+  delay(100);
+  sim800l.print("AT+CMGS=\"+919080659745\"\r");  //Your phone number don't forget to include your country code, example +212123456789"
+  delay(500);
+  sim800l.print("Power Consumed : "+String(val)); //This is the text to send to the phone number, don't make it too long or you have to modify the SoftwareSerial buffer
+  Serial.println("Power Consumed : "+String(val));
+  delay(500);
+  sim800l.print((char)26);// (required according to the datasheet)
+  delay(500);
+  sim800l.println();
+  Serial.println("Text Sent.");
+  delay(500);
 }
